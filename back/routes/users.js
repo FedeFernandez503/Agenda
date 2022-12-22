e31f2f596081159d30fee7d97880f92844c2648d
FedeFernandez503 <80352692+FedeFernandez503@users.noreply.github.com>
2022-12-21 21:06:23 -0300

    Delete back directory

--- back/routes/users.js
+++
@@ -1,7 +0,0 @@
-const route = require("express").Router();
-const userHandlers = require('../controllers/userController.js');
-
-    route.post('/createUser', userHandlers.createUser);
-    route.get('/getUsers', userHandlers.getUsers);
-
-module.exports = route;
